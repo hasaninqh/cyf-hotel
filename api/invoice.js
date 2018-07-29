9b19f818a1a1e2289ed5a3e6aedf679f5062c3e6
Seraphine Young <seraphine_ene@yahoo.com>
2018-07-29 17:42:11 +0100

    This is my first commit

--- api/invoice.js
+++ api/invoice.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+const invoices = require("../public/data/invoices.json");
+
+router.get("/:id?", (req, res) => {
+  if (req.params.id) {
+    const filter = invoices.filter(
+      invoice => invoice.id === parseInt(req.params.id)
+    );
+    res.status(200).json({
+      invoices: filter
+    });
+  } else {
+    res.status(200).json({
+      invoices: invoices
+    });
+  }
+});
+
 router.put("/invoice", (req, res) => {
   // TODO read req.query.reservationId and req.body.invoice and insert into DB
   res.status(200).json({
